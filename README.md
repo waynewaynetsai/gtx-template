ebe8d35a7e59768026e2d590968083377169d191
Shang-Wei Tsai <wayne040904007@gmail.com>
2020-08-02 09:28:47 +0800

    update reademe

--- README.md
+++ README.md
@@ -1,6 +1,64 @@
 # gtx-template
 
 [![Build status](https://travis-ci.com/ShangWeiTsai/gtx-template.svg?branch=master)](https://travis-ci.com/ShangWeiTsai/gtx-template.svg?branch=master)
-[![version](https://img.shields.io/npm/v/@tgenx/gtx-template?style=plastic)](https://www.npmjs.com/package/v/@tgenx/gtx-template)
 
 Elegant mirco template compiler engine for open source devtool
+
+### usage
+```js
+  const templ =  `
+       @-- dynamic conent from user input --@
+
+       <p> Hello @{{ username }} ! </p>
+
+       @-- dynamic conent from condition --@
+
+       @if(!username)
+        <p> Show [ if ] Block! </p>
+       @elseif(username==='anyname')
+        <p> Show [ else if ] Block! </p>
+       @else
+        <p> Show [ else ] Block! </p>
+       @endif
+
+       @-- dynamic conent from forloop --@
+
+       @for(let item of items)
+       <p> name: @{{item.name}} </p>
+       @endfor
+
+       @for(let key in items)
+       <p> @{{ key }} - @{{items[key].name}} </p>
+       @endfor
+   `;
+
+   const template = contentCompiler(templ, {
+     path: 'your_dirname/test.ts',
+     filename: 'test.ts',
+     options: {
+       delimiter: {
+         interpolate: {
+           enable: true,
+         },
+         escape: {
+           enable: false
+         }
+       }
+     }
+   });
+
+  const compiled = await template({
+    username: 'Wayne',
+    items: [
+        {
+            name: 'John'
+        },
+        {
+            name: 'Allen'
+        },
+        {
+            name: 'Judy'
+        }
+    ]
+  });
+```
--- example/demo.ts
+++ example/demo.ts
@@ -1,47 +1,50 @@
 import { contentCompiler } from "../src/compiler/compiler";
 
-const templ =  `
-@-- dynamic conent from user input --@
-
-<p> Hello @{{ username }} ! </p>
-
-@-- dynamic conent from condition --@
-
-@if(!username)
-<p> Show [ if ] Block! </p>
-@elseif(username==='anyname')
-<p> Show [ else if ] Block! </p>
-@else
-<p> Show [ else ] Block! </p>
-@endif
-
-@-- dynamic conent from forloop --@
-
-@for(let item of items)
-<p> name: @{{item.name}} </p>
-@endfor
-
-@for(let key in items)
- <p> @{{ key }} - @{{items[key].name}} </p>
-@endfor`;
-
-const template = contentCompiler(templ, {
- path: 'your_dirname/test.ts',
- filename: 'test.ts',
- options: {
-   delimiter: {
-     interpolate: {
-       enable: true,
-     },
-     escape: {
-       enable: false
-     }
-   }
- }
-});
 
 
 (async () => {
+
+    const templ =  `
+      @-- dynamic conent from user input --@
+
+      <p> Hello @{{ username }} ! </p>
+
+      @-- dynamic conent from condition --@
+
+      @if(!username)
+      <p> Show [ if ] Block! </p>
+      @elseif(username==='anyname')
+      <p> Show [ else if ] Block! </p>
+      @else
+      <p> Show [ else ] Block! </p>
+      @endif
+
+      @-- dynamic conent from forloop --@
+
+      @for(let item of items)
+      <p> name: @{{item.name}} </p>
+      @endfor
+
+      @for(let key in items)
+      <p> @{{ key }} - @{{items[key].name}} </p>
+      @endfor
+    `;
+
+    const template = contentCompiler(templ, {
+    path: 'your_dirname/test.ts',
+    filename: 'test.ts',
+    options: {
+      delimiter: {
+        interpolate: {
+          enable: true,
+        },
+        escape: {
+          enable: false
+        }
+      }
+    }
+    });
+
   const compiled = await template({
     username: 'Wayne',
     items: [
@@ -56,5 +59,5 @@ const template = contentCompiler(templ, {
         }
     ]
   });
-  console.log(compiled);
+
 })();
